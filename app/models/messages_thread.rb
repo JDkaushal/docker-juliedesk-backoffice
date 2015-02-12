ab19c998ae0cb438dc51a8a1d8c57d02a27ba066
Nicolas Marlier <nmarlier@gmail.com>
2015-02-12 12:22:05 +0100

    FIxed split

--- app/models/messages_thread.rb
+++ app/models/messages_thread.rb
@@ -144,7 +144,6 @@ class MessagesThread < ActiveRecord::Base
     updated_thread_id = nil
     google_messages.each do |google_message|
       google_message.threadId = updated_thread_id
-      google_message.labelIds = ['INBOX']
       updated_google_message = google_message.insert
       google_message.delete
       updated_thread_id = updated_google_message.thread_id
