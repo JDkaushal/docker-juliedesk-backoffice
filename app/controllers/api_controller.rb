de1388639c7a95afe9d8c709b594693b6ccdb525
Nicolas Marlier <nmarlier@gmail.com>
2015-10-07 11:27:03 +0200

    Better api

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,6 +1,10 @@
 class ApiController < ActionController::Base
 
-  before_filter :authenticate
+  before_filter :authenticate, :cors_preflight_check
+  after_filter :cors_set_access_control_headers
+  skip_before_filter :verify_authenticity_token
+
+  protected
 
   def authenticate
     unless params[:access_key] == "EDx19D72bH7e5I64EXk1kwa4jXvynddS"
@@ -11,5 +15,23 @@ class ApiController < ActionController::Base
       false
     end
     true
+  end
+
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
+    headers['Access-Control-Max-Age'] = "1728000"
+  end
+
+  def cors_preflight_check
+    if request.method == 'OPTIONS'
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+      headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
+      headers['Access-Control-Max-Age'] = '1728000'
+
+      render :text => '', :content_type => 'text/plain'
+    end
   end
 end
