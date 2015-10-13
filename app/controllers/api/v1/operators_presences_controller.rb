23775ccaffd11955330b2533f77f4549eb7f681d
Nicolas Marlier <nmarlier@gmail.com>
2015-10-13 17:22:31 +0200

    Better operator presences api

--- app/controllers/api/v1/operators_presences_controller.rb
+++ app/controllers/api/v1/operators_presences_controller.rb
@@ -9,6 +9,8 @@ class Api::V1::OperatorsPresencesController < Api::ApiV1Controller
     date = date.beginning_of_hour
 
     operator_presences = OperatorPresence.where(date: date).includes(:operator)
+
+    pusher_user_emails = Pusher.get("/channels/presence-global/users")[:users].map{|u| u['id']}
     render json: {
         status: "success",
         data: {
@@ -16,6 +18,8 @@ class Api::V1::OperatorsPresencesController < Api::ApiV1Controller
             operators: operator_presences.map{|op|
               {
                   name: op.operator.name,
+                  email: op.operator.email,
+                  present: pusher_user_emails.include?(op.operator.email),
                   operator_id: op.operator_id
               }
             }
