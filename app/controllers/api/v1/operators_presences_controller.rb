9c1b00d55751e9b9d2e7069cbd9e7329f2e9339f
Nicolas Marlier <nmarlier@gmail.com>
2015-10-13 16:10:47 +0200

    Added operator names to api

--- app/controllers/api/v1/operators_presences_controller.rb
+++ app/controllers/api/v1/operators_presences_controller.rb
@@ -7,10 +7,18 @@ class Api::V1::OperatorsPresencesController < Api::ApiV1Controller
       date = DateTime.now
     end
     date = date.beginning_of_hour
+
+    operator_presences = OperatorPresence.where(date: date).includes(:operator)
     render json: {
         status: "success",
         data: {
-            operators_count: OperatorPresence.where(date: date).count
+            operators_count: operator_presences.count,
+            operators: operator_presences.map{|op|
+              {
+                  name: op.operator.name,
+                  operator_id: op.operator_id
+              }
+            }
         }
     }
   end
