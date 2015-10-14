55c57a50ff3292a6c0be990e611c2ca7e3de659c
Nicolas Marlier <nmarlier@gmail.com>
2015-10-14 12:14:31 +0200

    Admin count in inbox count api

--- app/controllers/api/v1/messages_threads_controller.rb
+++ app/controllers/api/v1/messages_threads_controller.rb
@@ -4,14 +4,10 @@ class Api::V1::MessagesThreadsController < Api::ApiV1Controller
     messages_threads = MessagesThread.where(in_inbox: true)
     accounts_cache = Account.accounts_cache(mode: "light")
     messages_threads.each{|mt| mt.account(accounts_cache: accounts_cache)}
-    messages_threads = messages_threads.select{ |mt|
-        !mt.delegated_to_founders &&
-          mt.account &&
-          !mt.account.only_admin_can_process
-    }
+
 
     messages_threads = messages_threads.select do |mt|
-      if mt.account.company_hash
+      if mt.account && mt.account.company_hash
         working_hours = mt.account.company_hash['working_hours']
         timezone = mt.account.company_hash['timezone']
         now = DateTime.now.in_time_zone(timezone)
@@ -33,10 +29,23 @@ class Api::V1::MessagesThreadsController < Api::ApiV1Controller
       end
     end
 
+    inbox_messages_threads = messages_threads.select{ |mt|
+      !mt.delegated_to_founders &&
+          mt.account &&
+          !mt.account.only_admin_can_process
+    }
+
+    admin_messages_threads = messages_threads.select{ |mt|
+      mt.delegated_to_founders ||
+          !mt.account ||
+          mt.account.only_admin_can_process
+    }
+
     render json: {
         status: "success",
         data: {
-            count: messages_threads.length
+            count: inbox_messages_threads.length,
+            admin_count: admin_messages_threads.length
         }
     }
   end
