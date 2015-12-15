1cc3180b5a31142fd6b9d76b670550f2932ff89b
Nicolas Marlier <nmarlier@gmail.com>
2015-12-15 12:46:44 +0100

    Fixed event details for other accounts

--- app/assets/javascripts/calendar/main.js
+++ app/assets/javascripts/calendar/main.js
@@ -197,6 +197,15 @@ Calendar.prototype.allEmails = function() {
     return allEmails;
 };
 
+Calendar.prototype.findAccountEmailForEvent = function(event) {
+    var calendar = this;
+    var calendarLoginUsername = event.calendar_login_username;
+    var calendarObject = _.find(calendar.calendars, function(calendar) {
+        return calendar.calendar_login_username == calendarLoginUsername;
+    });
+    return calendarObject.email;
+};
+
 Calendar.prototype.fetchCalendars = function (callback) {
     var calendar = this;
     calendar.showLoadingSpinner();
