e02fcc2a35e9ffeec3bc71ebe80760c9495d31c7
Fred-JulieDesk <frederic@juliedesk.com>
2016-05-26 14:39:16 +0200

    Ai forecast now projection duration is editable through env variable + fetching forecast every sunday at 2AM (UTC) + updating forecast result when necessary

--- app/models/ai_email_flow_forecast.rb
+++ app/models/ai_email_flow_forecast.rb
@@ -18,26 +18,29 @@ class AiEmailFlowForecast < ActiveRecord::Base
         unless date.present?
           date = DateTime.now
         end
+      end
 
-     end
-
-    make_call(date)
+    make_call(date, ENV['AI_FORECAST_PROJECTION_DURATION_IN_DAYS'])
   end
 
   private
 
-  def self.make_call(date)
+  def self.make_call(date, duration)
     client = HTTPClient.new(default_header: {
                                 "Authorization" => ENV['CONSCIENCE_API_KEY']
                             })
     client.ssl_config.verify_mode = 0
-    url = "#{ENV['CONSCIENCE_API_BASE_PATH_V1']}/forecastemails/?date=#{date.strftime('%Y-%m-%d')}&duration=60"
+    url = "#{ENV['CONSCIENCE_API_BASE_PATH_V1']}/forecastemails/?date=#{date.strftime('%Y-%m-%d')}&duration=#{duration}"
     response = client.get(url)
 
     json_response = JSON.parse(response.body)
 
     json_response['forecast'].each do |datetime, count|
-      AiEmailFlowForecast.find_or_create_by(datetime: datetime, count: count)
+      if forecast = AiEmailFlowForecast.find_by(datetime: datetime)
+        forecast.update(count: count)
+      else
+        AiEmailFlowForecast.create(datetime: datetime, count: count)
+      end
     end
   end
 end
--- lib/tasks/ai_email_forecast.rake
+++ lib/tasks/ai_email_forecast.rake
@@ -1,7 +1,12 @@
 namespace :ai do
   task :email_forecast => :environment do |t|
+    now = Time.now
 
-    AiEmailFlowForecast.fetch
+    # We run the forecast every Sunday at 2AM (UTC)
+    # It will predict the email incoming flow for the next two weeks
+    if now.wday == 0
+      AiEmailFlowForecast.fetch(now)
+    end
 
     print('OK')
   end
