74f74827ceb8e10874d7c724017abca5b6c9f43b
Fred-JulieDesk <frederic@juliedesk.com>
2016-10-13 10:53:51 +0200

    Remobing puts

--- app/helpers/client_success_tracking_helpers.rb
+++ app/helpers/client_success_tracking_helpers.rb
@@ -15,19 +15,19 @@ class ClientSuccessTrackingHelpers
   end
 
   def self.track event_name, account_email, properties
-    puts "Going to track #{event_name} for user #{account_email}"
-    puts "Properties: #{properties}"
+    # puts "Going to track #{event_name} for user #{account_email}"
+    # puts "Properties: #{properties}"
 
     tracker = Mixpanel::Tracker.new(ENV['MIXPANEL_FOR_CLIENT_SUCCESS_TOKEN'])
     tracker.track("#{account_email}", event_name, properties)
 
-    puts "Mixpanel done, starting analytics"
-    puts ENV['SEGMENT_WRITE_KEY']
+    # puts "Mixpanel done, starting analytics"
+    # puts ENV['SEGMENT_WRITE_KEY']
     analytics = Segment::Analytics.new({write_key: ENV['SEGMENT_WRITE_KEY']})
-    puts analytics
+    #puts analytics
     result = analytics.track(user_id: "#{account_email}", event: event_name, properties: properties)
-    puts result
+    #puts result
 
-    puts "Done"
+    #puts "Done"
   end
 end
