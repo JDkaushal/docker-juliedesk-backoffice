960289b5deac048a40f77fd72220eba2807a1601
Nicolas Marlier <nicolas@juliedesk.com>
2016-11-21 14:01:41 +0100

    operators action of weekly recap follow up on computer

--- app/workers/generate_follow_up_worker.rb
+++ app/workers/generate_follow_up_worker.rb
@@ -9,18 +9,19 @@ class GenerateFollowUpWorker
     messages_thread = MessagesThread.find messages_thread_id
     messages_thread.update_attributes(should_follow_up: true, follow_up_instruction: instruction)
 
+    real_operator_id = Operator.find_by_email("computer@operator.juliedesk.com").id
     OperatorAction.create({
                               initiated_at: DateTime.now,
                               target: messages_thread,
                               nature: OperatorAction::NATURE_OPEN,
-                              operator_id: session[:operator_id],
+                              operator_id: real_operator_id,
                               messages_thread_id: messages_thread.id
                           })
     OperatorAction.create_and_verify({
                                          initiated_at: DateTime.now,
                                          target: messages_thread,
                                          nature: OperatorAction::NATURE_SEND_TO_SUPPORT,
-                                         operator_id: operator_id,
+                                         operator_id: real_operator_id,
                                          messages_thread_id: messages_thread.id,
                                          message: "#FollowUp #{instruction}"
                                      })
