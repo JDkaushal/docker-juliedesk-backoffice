9ffbd56b5a14fb5a12e0032e2b6b4cdbc9c7f0e4
Nicolas Marlier <nicolas@juliedesk.com>
2016-11-30 19:52:09 +0100

    Template generation

--- app/assets/javascripts/generate_template_via_service.js
+++ app/assets/javascripts/generate_template_via_service.js
@@ -1,23 +1,48 @@
 window.generateEmailTemplateViaService = function(params, callback) {
 
-    var dataForService = {
-        client_names: params.client_names,
-        dates: params.dates,
-        timezones: params.timezones,
-        default_timezone: params.default_timezone,
-        locale: params.locale,
-        is_virtual: params.is_virtual,
-        attendees: params.attendees,
-        appointment_in_email: params.appointment_in_email,
-        location_in_email: params.location_in_email,
-        should_ask_location: params.should_ask_location,
-        missing_contact_info: params.missing_contact_info
-    };
+    var dataForService = {};
 
+    if(params.template == "suggest_dates") {
+        dataForService = {
+            client_names: params.client_names,
+            timezones: params.timezones,
+            default_timezone: params.default_timezone,
+            locale: params.locale,
+            is_virtual: params.is_virtual,
+            attendees: params.attendees,
+            appointment_in_email: params.appointment_in_email,
+            location_in_email: params.location_in_email,
+            should_ask_location: params.should_ask_location,
+            missing_contact_info: params.missing_contact_info,
+
+            dates: params.dates
+        };
+    }
+    else if(params.template == "send_invitations") {
+        dataForService = {
+            client_names: params.client_names,
+            timezones: params.timezones,
+            default_timezone: params.default_timezone,
+            locale: params.locale,
+            is_virtual: params.is_virtual,
+            attendees: params.attendees,
+            appointment_in_email: params.appointment_in_email,
+            location_in_email: params.location_in_email,
+            should_ask_location: params.should_ask_location,
+            missing_contact_info: params.missing_contact_info,
+
+            date: params.date,
+            location_kind: params.location_kind
+        }
+    }
+    else {
+        return null;
+    }
 
     $.ajax({
         type : "POST",
         url :  "https://template-generator.juliedesk.net/api/v1/templates/suggest_dates",
+        // url :  "http://localhost:4567/api/v1/templates/" + params.template,
         dataType: 'json',
         contentType: 'application/json',
         data : JSON.stringify(dataForService),
@@ -25,4 +50,5 @@ window.generateEmailTemplateViaService = function(params, callback) {
             callback(response.data);
         }
     });
+
 };
