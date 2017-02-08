d47c0d26c98f21064ca499c65d4ed885a4e3946c
Vladan Djeric <vladan@juliedesk.com>
2017-02-08 09:56:29 +0100

    fix

--- app/assets/javascripts/angular/reply_box/reply-box-controllers.js
+++ app/assets/javascripts/angular/reply_box/reply-box-controllers.js
@@ -64,10 +64,10 @@
                         });
 
                         // Include julie aliases (except the main one)
-                        _.each(window.julie_alias_emails, function(julie_alias_email) {
-                            if(window.julieAlias && julie_alias_email != window.julieAlias.email)
-                                $scope.ccs.push({ name: julie_alias_email } );
-                        });
+                        //_.each(window.julie_alias_emails, function(julie_alias_email) {
+                        //    if(window.julieAlias && julie_alias_email != window.julieAlias.email)
+                        //        $scope.ccs.push({ name: julie_alias_email } );
+                        //});
                     }
 
                     var tos = tos_attendees.map(function (attendee) {
