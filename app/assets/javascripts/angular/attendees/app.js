a16bbf4ef9ab66f5ec0d04d008ea0cf7dceb815a
Fred-JulieDesk <frederic@juliedesk.com>
2017-03-24 12:26:59 +0100

    Bootstraping attendees angular app via JS and not via directive ng-app anymore

--- app/assets/javascripts/angular/attendees/app.js
+++ app/assets/javascripts/angular/attendees/app.js
@@ -1,5 +1,7 @@
 (function(){
-
     var app = angular.module('attendees-manager', ['attendees-manager-directives', 'attendees-manager-services', 'attendees-manager-controllers', 'attendees-manager-filters']);
 
+    angular.element(document).ready(function () {
+        angular.bootstrap(document.getElementById("attendees_manager_app"),['attendees-manager']);
+    });
 })();
