624a9d105690e5341bb7689f602457d2d4944793
Fred-JulieDesk <frederic@juliedesk.com>
2017-03-30 18:25:08 +0200

    Fix parse custom attendees

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -46,7 +46,7 @@ module ApplicationHelper
     letter_regexp = /(?:#{(letters + accented_characters).join("|")})/
 
     inside_email_regexp = /(?:[a-zA-Z0-9]|\#)(?:[a-zA-Z0-9]|\.|\-|\+|\_)*@(?:[a-zA-Z0-9]|\-|\.)*(?:\.[a-zA-Z]*){1,2}/
-    inside_name_regexp = /(?:#{letter_regexp}|\#)(?:#{letter_regexp}|\-|\ |\'|\>|\+|\.|[0-9]|\&|\?|\/)*/
+    inside_name_regexp = /(?:#{letter_regexp}|\#)(?:#{letter_regexp}|\-|\ |\'|\’|\>|\+|\.|[0-9]|\&|\?|\/)*/
     email_regexp = /(?<email>#{inside_email_regexp})/
     name_regexp = /(?<name>#{inside_name_regexp})(?:\ \(.*\))?/
 
