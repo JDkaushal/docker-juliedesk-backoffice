4bef9661aaefc6a3be26d119aca28bc734a84fc0
Nicolas Marlier <nicolas@juliedesk.com>
2017-08-08 10:21:22 +0200

    Ability to see human overiide on date suggestion review

--- app/assets/javascripts/angular/date_suggestions_full_auto_review_manager/date-suggestions-full-auto-review-controllers.js
+++ app/assets/javascripts/angular/date_suggestions_full_auto_review_manager/date-suggestions-full-auto-review-controllers.js
@@ -34,6 +34,25 @@
             window.open("/review/date_suggestions_reviews/" + dateSuggestionsReview.id + "/full_auto");
         };
 
+        $scope.fetchFromConscience = function() {
+            $scope.conscienceLoading = true;
+            console.log($scope.julieActions);
+            console.log(_.map($scope.julieActions, function(suggestedDate) { return suggestedDate.julie_action_id }).join(","));
+            conscienceApi.suggestedDatesListRequest({
+                julie_action_ids: _.map($scope.julieActions, function(suggestedDate) { return suggestedDate.julie_action_id }).join(",")
+            }).then(function (response) {
+                var conscienceSuggestedDates = response.data.data.suggested_dates;
+                console.log(conscienceSuggestedDates);
+                _.each($scope.julieActions, function(suggestedDate) {
+                    var conscienceJulieAction = _.find(conscienceSuggestedDates, function(conscienceSuggestedDatesItem) {
+                        return conscienceSuggestedDatesItem.julie_action_id == suggestedDate.julie_action_id
+                    });
+                    suggestedDate.auto_process_force_human_reason = conscienceJulieAction.auto_process_force_human_reason;
+                    suggestedDate.auto_process_force_human_reason_details = conscienceJulieAction.auto_process_force_human_reason_details;
+                });
+                $scope.conscienceLoading = false;
+            });
+        };
 
 
 
@@ -51,6 +70,8 @@
                 $scope.julieActions = response.data.data.julie_actions;
 
                 $scope.loading = false;
+
+                $scope.fetchFromConscience();
             });
         };
 
