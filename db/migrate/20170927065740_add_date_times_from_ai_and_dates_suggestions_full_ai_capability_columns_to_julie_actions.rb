037cdf1b09acebc11d9aa8abe9a81eb69c151b93
Nicolas Marlier <nicolas@juliedesk.com>
2017-10-09 10:55:43 +0200

    Re fix for migrations

--- db/migrate/20170927065740_add_date_times_from_ai_and_dates_suggestions_full_ai_capability_columns_to_julie_actions.rb
+++ db/migrate/20170927065740_add_date_times_from_ai_and_dates_suggestions_full_ai_capability_columns_to_julie_actions.rb
@@ -1,5 +1,5 @@
 class AddDateTimesFromAiAndDatesSuggestionsFullAiCapabilityColumnsToJulieActions < ActiveRecord::Migration
   def change
-    add_column :julie_actions, :date_times_from_ai, :json
+    add_column :julie_actions, :date_times_from_ai, :string
   end
 end
