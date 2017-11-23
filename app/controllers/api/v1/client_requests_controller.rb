b20803274a8c2967e315006ebc2a41b554fbef5b
Nicolas Marlier <nicolas@juliedesk.com>
2017-11-23 17:50:43 +0100

    New route for client requests

---
+++ app/controllers/api/v1/client_requests_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::ClientRequestsController < Api::ApiV1Controller
+
+  def compute_count
+    data = ClientRequest.compute_count(params[:user_ids], params[:team_identifiers], {
+        detailed: params[:detailed].present?
+    }, DateTime.parse(params[:from_date]), DateTime.parse(params[:to_date]))
+
+    render json: {
+        status: 'success',
+        data: data
+    }
+  end
+end
