af7323ff2d6f8fb99cbd8875d3bdf4bcd65e5b44
Nicolas Marlier <nicolas@juliedesk.com>
2017-12-19 15:54:31 +0100

    Geo zones update for angelique

--- app/controllers/geo_zones_controller.rb
+++ app/controllers/geo_zones_controller.rb
@@ -6,10 +6,9 @@ class GeoZonesController < ActionController::Base
     # Dirty static modification for Engie, as a POC. This has to integrated in a cleaner way in the future
     all_client_specific_zones = {
         'angelique.stoffels@engie.com' => [
-            'ZONE 1 _ LIEGE CENTRE',
-            'ZONE 2 _ LIEGE PERIPHERIE',
-            'ZONE 3 _ CANTONS DE L\'EST',
-            'ZONE 4 - NAMUROIS'
+            'REGION DE NAMUR',
+            'CANTONS DE L\'EST',
+            'PERIPHERIE DE LIEGE'
         ]
     }
     client_specific_zones = (all_client_specific_zones["#{params[:account_email]}"] || []).select{|zone|
