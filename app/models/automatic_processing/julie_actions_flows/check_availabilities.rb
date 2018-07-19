92776fe305da8a4bc1efbd8d62cdae29b6e14383
Vladan <vladan@juliedesk.com>
2018-07-19 18:48:18 +0200

    tracking

--- app/models/automatic_processing/julie_actions_flows/check_availabilities.rb
+++ app/models/automatic_processing/julie_actions_flows/check_availabilities.rb
@@ -83,6 +83,7 @@ module AutomaticProcessing
         puts params.inspect
 
         create_params = {
+          full_auto: true,
           messages_thread_id: @data_holder.get_messages_thread.try(:id),
           email: @data_holder.get_thread_owner_account.email,
           summary: @data_holder.get_message_classification_summary,
