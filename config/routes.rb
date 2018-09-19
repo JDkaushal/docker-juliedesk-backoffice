e00e61c74eb22483a56b308879266e22f7bed123
ferdinandroth <ferdinand@juliedesk.com>
2018-09-19 11:17:28 +0200

    Add time constraints review

--- config/routes.rb
+++ config/routes.rb
@@ -188,6 +188,13 @@ Rails.application.routes.draw do
       end
     end
 
+    resources :constraints, only: [] do
+      collection do
+        get "", action: :main
+        get "review", action: :review
+      end
+    end
+
 
     resources :messages_threads, only: [] do
       member do
