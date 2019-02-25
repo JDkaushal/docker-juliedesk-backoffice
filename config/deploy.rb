a325d0716974d0f550f9539175801cf525c8879f
Xavier Vanmeerbeck <xavier@juliedesk.com>
2019-02-25 15:58:11 +0100

    Bump capistrano ruby to 2.6.1

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,7 +17,7 @@ set :assets_roles, [:web, :app, :test]
 
 set :linked_dirs, %w{log tmp/pids script}
 set :linked_files, %w{config/database.yml .env}
-set :rvm_ruby_version, '2.4.4'
+set :rvm_ruby_version, '2.6.1'
 
 
 # Default value for :scm is :git
