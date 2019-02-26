9d84c3b9515df46ec87f4cec5264914949e4ed5c
Xavier Vanmeerbeck <xavier@juliedesk.com>
2019-02-26 11:38:54 +0100

    Use eager loading instead of autoloading

--- config/application.rb
+++ config/application.rb
@@ -12,8 +12,8 @@ module JuliedeskBackoffice
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.autoload_paths << Rails.root.join('lib')
-    config.autoload_paths << Rails.root.join('lib/*')
+    config.eager_load_paths << Rails.root.join('lib')
+    config.eager_load_paths << Rails.root.join('lib/*')
     #config.autoload_paths += Dir["#{config.root}/lib/**/"]
 
     config.angular_templates.ignore_prefix = %w(angular_templates/)
