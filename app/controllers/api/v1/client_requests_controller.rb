334651f1f9ba4417598af9b2f906ce0f79b258d6
Xavier Vanmeerbeck <xavier@juliedesk.com>
2019-02-26 14:38:37 +0100

    Fix compute_count

--- app/controllers/api/v1/client_requests_controller.rb
+++ app/controllers/api/v1/client_requests_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::ClientRequestsController < Api::ApiV1Controller
 
   def compute_count
+    params.permit!
     data = ClientRequest.compute_count(params[:user_ids], params[:team_identifiers], {
         detailed: params[:detailed].present?
     }, DateTime.parse(params[:from_date]), DateTime.parse(params[:to_date]))
